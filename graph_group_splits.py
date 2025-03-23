5b3ee29b003c440791a77a15109b52f5b01ac5bd
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2025-03-23 21:55:26 +0000

    Fix some stuff

--- graph_group_splits.py
+++ graph_group_splits.py
@@ -6,11 +6,15 @@ import csv
 
 attendees = {}
 
-with open('attendees.csv') as attendees_csv:
-    csv_reader = csv.reader(attendees_csv, delimiter=',')
+with open("attendees.csv") as attendees_csv:
+    csv_reader = csv.reader(attendees_csv, delimiter=",")
     next(csv_reader, None)
     for row in csv_reader:
-        attendees[row[5]] = [] + ([] if (not row[12] or "N/A" in row[12]) else [row[12]]) + ([] if (not row[13] or "N/A" in row[13]) else [row[13]])
+        attendees[row[5]] = (
+            []
+            + ([] if (not row[12] or "N/A" in row[12]) else [row[12]])
+            + ([] if (not row[13] or "N/A" in row[13]) else [row[13]])
+        )
 
 G = nx.DiGraph()
 G.add_nodes_from(attendees.keys())
@@ -26,30 +30,54 @@ for index, SG in enumerate(nx.connected_components(G.to_undirected())):
     print(SG)
 
 degrees = dict(G.degree)
-nodes = G.nodes()
-edge_labels = nx.get_edge_attributes(G,'weight')
-pos = nx.nx_pydot.graphviz_layout(G)
+nodes: nx.reportviews.NodeView = G.nodes()
+edge_labels: dict[tuple[str, str], int] = nx.get_edge_attributes(G, "weight")
+pos: dict[str, tuple[float, float]] = nx.nx_pydot.graphviz_layout(G)
 
-partition = com.best_partition(G.to_undirected(), resolution=0.2)
-cmap = mpl.colormaps['hsv']
+partition: dict[str, int] = com.best_partition(G.to_undirected(), resolution=0.2)
+cmap: plt.Colormap = mpl.colormaps["hsv"]
 
 
 plt.figure(figsize=(30, 30))
-plt.axis('off')
+plt.axis("off")
 
-nx.draw_networkx_edges(G, pos, width=1) 
+nx.draw_networkx_edges(G, pos, width=1)
 nx.draw_networkx_edge_labels(G, pos, edge_labels, font_size=7)
 
-nx.draw_networkx_nodes(G, pos, partition.keys(), 
-                       cmap=cmap, node_color=list(partition.values()),
-                       node_size=[100 + v * 100 for v in degrees.values()],
-                       alpha=0.8)
+nx.draw_networkx_nodes(
+    G=G,
+    pos=pos,
+    nodelist=partition.keys(),
+    cmap=cmap,
+    node_color=list(partition.values()),
+    node_size=[100 + v * 100 for v in degrees.values()],
+    alpha=0.8,
+)
 
+node: str
+x: float
+y: float
 for node, (x, y) in pos.items():
     if node in attendees:
-        plt.text(x, y - 0.1, node, ha='center', va='center', color='#000000', fontsize=10)
+        plt.text(
+            x=x,
+            y=y - 0.1,
+            s=node,
+            ha="center",
+            va="center",
+            color="#000000",
+            fontsize=10,
+        )
     else:
-        plt.text(x, y - 0.1, node, ha='center', va='center', color='#ff0000', fontsize=10)
+        plt.text(
+            x=x,
+            y=y - 0.1,
+            s=node,
+            ha="center",
+            va="center",
+            color="#ff0000",
+            fontsize=10,
+        )
 
-plt.savefig('graph.png', dpi=300, bbox_inches='tight')
+plt.savefig("graph.png", dpi=300, bbox_inches="tight")
 plt.show()
