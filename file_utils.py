c954daa83946d606c549724cf85c64d1db9430b7
Matty Widdop <18513864+MattyTheHacker@users.noreply.github.com>
2026-02-28 14:22:21 +0000

    Improve file handling

--- file_utils.py
+++ file_utils.py
@@ -1,31 +1,27 @@
 import csv
 
+
 attendees: set[str] = set()
 named_attendees: set[str] = set()
 edges: list[tuple[str, str]] = []
 
-
 def import_data() -> dict[str, list[str]]:
     attendees_and_preferences: dict[str, list[str]] = {}
     with open("data/attendees.csv") as attendees_csv:
-        csv_reader = csv.reader(attendees_csv, delimiter=",")
+        csv_reader = csv.DictReader(attendees_csv, delimiter=",")
         next(csv_reader, None)
         for row in csv_reader:
-            attendees_and_preferences[row[5]] = (
+            attendees_and_preferences[row["Name"]] = (
                 []
-                + ([] if (not row[14] or "N/A" in row[14]) else [row[14]])
-                + ([] if (not row[15] or "N/A" in row[15]) else [row[15]])
+                + ([] if (not row["Seat Pref 1"] or "N/A" in row["Seat Pref 1"]) else [row["Seat Pref 1"]])
+                + ([] if (not row["Seat Pref 2"] or "N/A" in row["Seat Pref 2"]) else [row["Seat Pref 2"]])
             )
 
-            attendees.add(row[5])
-            named_attendees.add(row[14]) if row[14] != "N/A" and row[
-                14
-            ] not in named_attendees else None
-            named_attendees.add(row[15]) if row[15] != "N/A" and row[
-                15
-            ] not in named_attendees else None
-            edges.append((row[5], row[14])) if row[14] != "N/A" else None
-            edges.append((row[5], row[15])) if row[15] != "N/A" else None
+            attendees.add(row["Name"])
+            named_attendees.add(row["Seat Pref 1"]) if row["Seat Pref 1"] != "N/A" and row["Seat Pref 1"] not in named_attendees else None
+            named_attendees.add(row["Seat Pref 2"]) if row["Seat Pref 2"] != "N/A" and row["Seat Pref 2"] not in named_attendees else None
+            edges.append((row["Name"], row["Seat Pref 1"])) if row["Seat Pref 1"] != "N/A" else None
+            edges.append((row["Name"], row["Seat Pref 2"])) if row["Seat Pref 2"] != "N/A" else None
 
     print(attendees_and_preferences)
 
