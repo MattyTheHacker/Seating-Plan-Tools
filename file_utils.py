f22a4bdcd73ef0ac4464499f813f39d2fc96842e
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2026-02-28 13:46:03 +0000

    Fix column numbers

--- file_utils.py
+++ file_utils.py
@@ -13,19 +13,21 @@ def import_data() -> dict[str, list[str]]:
         for row in csv_reader:
             attendees_and_preferences[row[5]] = (
                 []
-                + ([] if (not row[12] or "N/A" in row[12]) else [row[12]])
-                + ([] if (not row[13] or "N/A" in row[13]) else [row[13]])
+                + ([] if (not row[14] or "N/A" in row[14]) else [row[14]])
+                + ([] if (not row[15] or "N/A" in row[15]) else [row[15]])
             )
 
             attendees.add(row[5])
-            named_attendees.add(row[12]) if row[12] != "N/A" and row[
-                12
+            named_attendees.add(row[14]) if row[14] != "N/A" and row[
+                14
             ] not in named_attendees else None
-            named_attendees.add(row[13]) if row[13] != "N/A" and row[
-                13
+            named_attendees.add(row[15]) if row[15] != "N/A" and row[
+                15
             ] not in named_attendees else None
-            edges.append((row[5], row[12])) if row[12] != "N/A" else None
-            edges.append((row[5], row[13])) if row[13] != "N/A" else None
+            edges.append((row[5], row[14])) if row[14] != "N/A" else None
+            edges.append((row[5], row[15])) if row[15] != "N/A" else None
+
+    print(attendees_and_preferences)
 
     return attendees_and_preferences
 
